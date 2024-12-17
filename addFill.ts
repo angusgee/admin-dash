e913bcc82e3eb614ff3a420f9c1411ddc3e8b89e
MaltaWebDev <MaltaWebDev@gmail.com>
2024-12-17 23:47:02 +0100

    fix: all SVG files need white fill added pt II
    - update script to iterate over all svg files
    - run script and confirm files updated

--- addFill.ts
+++ addFill.ts
@@ -1,19 +1,34 @@
-const fs = require('node:fs');
+const fs = require('node:fs').promises;
+const path = require('node:path');
 
-const content: string = fs.readFile('./icons/cog.svg', 'utf8', (err, data: string) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  const updatedContent = data.replace("<path ", '<path fill="#FFFFFF" ')
-  fs.writeFile('./icons/cog.svg', updatedContent, err => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log("file written successfully!");
+async function addFillToSVGs(): Promise<void> {
+  try {
+    const iconDir = './icons'
+    const files = await fs.readdir(iconDir)
+    console.log("Files found: ", files);
+
+    const svgFiles = files.filter(file => path.extname(file).toLowerCase() === '.svg')
+
+    console.log("SVG files identified: ", svgFiles);
+
+    for (const file of svgFiles) {
+      const filepath = path.join(iconDir, file)
+      console.log("filepath: ", filepath);
+      const data: string = await fs.readFile(filepath, 'utf8')
+
+      if (!data.includes('fill="')) {
+        const updatedContent = data.replace("<path ", '<path fill="#FFFFFF" ')
+        await fs.writeFile(filepath, updatedContent)
+        console.log("Successfully updated: ", file);
+      }
+
     }
-  });
-});
 
+    console.log("All files processed!");
 
+  } catch (error) {
+    console.log("Error processing files: ", error);
+  }
+}
 
+addFillToSVGs()
